fbf3338ad0e328fc0840a2d85857c9d363832030
Mike Beck <virtualbeck@gmail.com>
2023-06-22 09:28:44 -0500

    working example

--- resources/table.go
+++ resources/table.go
@@ -3,10 +3,19 @@ package resources
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/TheJumpCloud/jcapi"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/cloudquery/plugin-sdk/v3/transformers"
+)
+
+var (
+	apiURL      = config("JUMPCLOUD_API_URL", "https://console.jumpcloud.com/api")
+	apiKey      = config("JUMPCLOUD_API_KEY", "")
+	apiClientV1 = jcapi.NewJCAPI(apiKey, apiURL)
+	isGroups    = true //IDK What this is
 )
 
 func UsersTable() *schema.Table {
@@ -18,5 +27,19 @@ func UsersTable() *schema.Table {
 }
 
 func fetchUsers(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
+	userList, err := apiClientV1.GetSystemUsers(!isGroups)
+	if err != nil {
+		log.Fatalf("Could not read system users, err='%s'\n", err)
+	}
+	fmt.Println(userList)
+
 	return fmt.Errorf("not implemented")
+}
+
+func config(s, e string) string {
+	envVar := os.Getenv(s)
+	if envVar != "" {
+		return envVar
+	}
+	return e
 }
